db8244d5ca9db164aa45088849859592721a5006
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-11 14:29:17 -0600

    Add files via upload

---
+++ Dec11.js
@@ -0,0 +1,11 @@
+const now = 2031
+const ageJazmine = now - 2020;
+const ageYasmine = now - 2021;
+
+console.log(now - 2020 > now - 2021);
+
+x = 7 = - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJazmine = ageYasmine) / 2
+console.log(ageJazmine, ageYasmine, averageAge);
---
+++ Dec11Part2.js
@@ -0,0 +1,10 @@
+const massJazz = 95;
+const heightJazz = 1.88;
+const massYazz = 85;
+const heightYazz = 1.76;
+
+const BMIJazz = massJazz / heightJazz ** 2;
+const BMIYazz = massYazz / (heightYazz * heightYazz);
+const jazzhigherBIM = BIMMark > BMIYazz;
+
+console.log(BIMJazz, BIMYazz, jazzHigherBIM);
